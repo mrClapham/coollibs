2882440900a4eb266cc27e18ea0913658d760685
Graham Clapham <info@gcdesign.co.uk>
2013-10-17 22:35:54 +0100

    Now drawing the Canvas

--- app/scripts/easleBase.js
+++ app/scripts/easleBase.js
@@ -32,10 +32,76 @@ require(['jquery','d3', 'easel', 'circCollision', 'angular' ], function($, d3, e
 
     //-----
 
+    function fibonaciSpiral(){
+        this.maxLeaves_     = 300
+        // golden mea proportion
+        this.g_             = 1/1.618033989;
+        this.ga_            = 360 - 360 * this.g_;
+        // The starting radius
+        this.rad_           = 20;
+        this.rGrowth_       = 1.005;
+        this.cur_           = this.maxLeaves_;
+        this.rot_           = 0;
+        this.width          = 600;
+        this.height         = 600;
+        this.timer          = null;
+        this.targ           = null;
+        this.canvas         = null;
+    }
+
+    fibonaciSpiral.prototype.setTarget = function(targ){
+
+        if(document.getElementById(targ)){
+            this.targ=document.getElementById(targ);
+            this.render();
+        }
+    }
+
+    fibonaciSpiral.prototype.render = function(){
+        this.targ.setAttribute('width', this.width+'px');
+        this.targ.setAttribute('height', this.height+'px');
+        this.targ.setAttribute('background-color', '#ff00ff');
+
+
+        var canvas = document.createElement('canvas');
+        canvas.id     = "CursorLayer";
+        canvas.width  = this.width;
+        canvas.height = this.height;
+        canvas.style.zIndex   = 8;
+        canvas.style.position = "absolute";
+        canvas.style.border   = "1px solid";
+
+
+        this.targ.appendChild(canvas);
+    }
+
+    fibonaciSpiral.prototype.onFrame = function(){
+        if(this.cur_){
+            this.cur_= -this.cur_;
+        }
+        this.rot_ += this.ga_;
+        this.rot -= parseInt(this.rot_ / 360) * 360;
+        this.rad *= this.rGrowth_ ;
+        var x = Math.cos(this.rot_ * Math.PI * 180) * this.rad_;
+        var y = Math.sin(this.rot_ * Math.PI * 180) * this.rad_;
+        console.log("rot leaves ",x)
+    }
+
+    fibonaciSpiral.prototype.startTimer = function(){
+        var this_ = this
+        this.timer = setInterval(function(){
+            this_.onFrame();
+        }, 1000)
+    }
+
+    var _fib = new fibonaciSpiral()
+    _fib.setTarget("fibDiv")
+    _fib.startTimer();
+
     var circCollis = new circ()
 
 
-    console.log("Hello world", circCollis)
+    console.log("Hello wide world", _fib)
 
 
 });
