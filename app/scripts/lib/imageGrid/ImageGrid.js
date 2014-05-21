c1985ab6f7ec524934aa24644065df09acda5dab
Graham Clapham <graham.clapham@thomsonreuters.com>
2014-05-21 15:11:45 +0100

    The grid function now working

--- app/scripts/lib/imageGrid/ImageGrid.js
+++ app/scripts/lib/imageGrid/ImageGrid.js
@@ -2,15 +2,20 @@
  * Created by grahamclapham on 19/05/2014.
  */
 var ImageGrid = (function (name){
+
+
+    var _scope = function(n, width, height, opt_target){
+
         //Non public functions
-        var _private = {
+        this._private = {
             _name:name,
+            _dispatcher:null,
             _image:null,
             _imageSrc:'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg',
             _ImageData:null,
             _gridSpacing:4,
-            _width:200,
-            _height:200,
+            _width:width ? width : 200,
+            _height:height ? height : 200,
             _imageWidth:null,
             _imageHeight:null,
             _canvas:null,
@@ -19,123 +24,132 @@ var ImageGrid = (function (name){
             _outputContext:null,
             _opt_target:null,
             _grid:[],
-            _onGridSpacingChanged:function(){
-            // console.log("_onGridSpacingChanged ",_private._gridSpacing+ " ",  _private._name, this.getWidth()  )
-            },
-            _onImageSet:function(){
-                _private._image = new Image();
-                _private._image.onload = function() {
-                    _private._context.drawImage(_private._image, 0, 0);
-                    _private._ImageData = ImageGrid.getImageData(_private._context,0,0, _private._width, _private._height);
-                    //console.log(_private._ImageData)
-                    //console.log(_private._ImageData)
-
-                    _private._makeImageGrid()
-
-                };
-                console.log(_private._context);
-                _private._image.src = _private._imageSrc;
-            },
-            _makeImageGrid:function(){
-
-                _private._grid = [];
-                var _xDivisions = parseInt(_private._ImageData.width / _private._gridSpacing);
-                var _yDivisions = parseInt(_private._ImageData.height / _private._gridSpacing);
-                    console.log("Width ",_private._ImageData.width)
-                    console.log("height",_private._ImageData.height)
-                    console.log("DIVISIONS    ",_xDivisions, _yDivisions);
-
-                //blank out the canvas
+        };
 
 
-                var step = _private._gridSpacing;
-                for(var i=0; i<_yDivisions; i++){
-                    _private._grid.push([])
-                    for(var n=0; n<_xDivisions; n++){
-                        var dt =  ImageGrid.getImageData(_private._context, step*n, step*i, step, step);
-                        var _pixelObject = {r:dt.data[0], g:dt.data[1], b:dt.data[2], a:dt.data[3] }
-                        _private._grid[i].push( _pixelObject );
-                        // console.log(_pixelObject );
-                        // ImageGrid.setPixel(_private._ImageData,30,30, dt.data[0], dt.data[1], dt.data[2], dt.data[3] )
-                        // _private._context.putImageData(dt, step*i, 30);
-                        // _private._context.fillStyle="rgba("+dt.data[0]+","+ dt.data[1]+","+dt.data[2]+","+dt.data[3]+")";
-                        //_private._context.fillStyle=ImageGrid.rgbToGreyscale(dt.data[0],dt.data[1],dt.data[2]);
-                       // _private._context.fillRect(step*n, step*i,_private._gridSpacing,_private._gridSpacing);
-                        var _rgbValue = "rgba("+dt.data[0]+","+ dt.data[1]+","+dt.data[2]+","+dt.data[3]+")";
-                        ImageGrid.drawCircle(_private._outputContext, step*n, step*i,_private._gridSpacing/2 , ImageGrid.rgbToGreyscale(dt.data[0],dt.data[1],dt.data[2]), true)
-                        ImageGrid.drawCircle(_private._outputContext, step*n, step*i,(_private._gridSpacing/2) -3 , _rgbValue, false)
-                    }
-                }
-                //console.log(_private._grid)
-            },
-            _onTargetSet:function(){
-                if(_private._opt_target){
-                    _private._opt_target.appendChild(_private._canvas)
-                    _private._opt_target.appendChild(_private._outputCanvas)
-
-                }
-            }
-    };
-
-    var _scope = function(n, width, height, opt_target){
-        _private._name = n;
-        _private._width = width;
-        _private._height = height;
+        this._private._name = n;
+        this._private._width = width;
+        this._private._height = height;
+        this._private._dispatcher = document.createElement('div');
         console.log(opt_target)
-        if(opt_target) _private._opt_target = opt_target;
+        if(opt_target) this._private._opt_target = opt_target;
 
-   // private functions
-   var  init = function(){
-       _private._canvas = ImageGrid.makeCanvas(_private._width, _private._height, "newCanv");
-       _private._context = _private._canvas.getContext('2d');
+        this._private._canvas = ImageGrid.makeCanvas(this._private._width, this._private._height, "newCanv");
+        this._private._context = this._private._canvas.getContext('2d');
+        console.log( this._private._context  )
+        this._private._outputCanvas = ImageGrid.makeCanvas(this._private._width, this._private._height, "newCanv");
+        this._private._outputContext = this._private._outputCanvas.getContext('2d');
 
-       _private._outputCanvas = ImageGrid.makeCanvas(_private._width, _private._height, "newCanv");
-       _private._outputContext = _private._outputCanvas.getContext('2d');
-       }
         init.call(this);
     };
 
+    // private functions
+    var  init = function(){
+//        this._private._canvas = ImageGrid.makeCanvas(this._private._width, this._private._height, "newCanv");
+//        this._private._context = this._private._canvas.getContext('2d');
+//        console.log( this._private._context  )
+//        this._private._outputCanvas = ImageGrid.makeCanvas(this._private._width, this._private._height, "newCanv");
+//        this._private._outputContext = this._private._outputCanvas.getContext('2d');
+    }
+
+    var _onTargetSet = function(){
+        if(this._private._opt_target){
+            this._private._opt_target.appendChild(this._private._canvas)
+            this._private._opt_target.appendChild(this._private._outputCanvas)
+        }
+    };
+
+    //////////
+    var _onGridSpacingChanged = function(){
+        // console.log("_onGridSpacingChanged ",this._private._gridSpacing+ " ",  this._private._name, this.getWidth()  )
+    }
+
+    var _makeImageGrid = function(){
+        this._private._grid = [];
+        var _xDivisions = parseInt(this._private._ImageData.width / this._private._gridSpacing);
+        var _yDivisions = parseInt(this._private._ImageData.height / this._private._gridSpacing);
+        console.log("Name ",this._private._name)
+        console.log("Width ",this._private._ImageData.width)
+        console.log("height",this._private._ImageData.height)
+        console.log("DIVISIONS    ",_xDivisions, _yDivisions);
+
+        var step = this._private._gridSpacing;
+        for(var i=0; i<_yDivisions; i++){
+            this._private._grid.push([])
+            for(var n=0; n<_xDivisions; n++){
+                var dt =  ImageGrid.getImageData(this._private._context, step*n, step*i, step, step);
+                var _pixelObject = {r:dt.data[0], g:dt.data[1], b:dt.data[2], a:dt.data[3] }
+                this._private._grid[i].push( _pixelObject );
+                var _rgbValue = "rgba("+dt.data[0]+","+ dt.data[1]+","+dt.data[2]+","+dt.data[3]+")";
+                ImageGrid.drawCircle(this._private._outputContext, step*n, step*i,this._private._gridSpacing/2 , ImageGrid.rgbToGreyscale(dt.data[0],dt.data[1],dt.data[2]), true)
+                ImageGrid.drawCircle(this._private._outputContext, step*n, step*i,(this._private._gridSpacing/2) -2 , _rgbValue, true)
+            }
+        }
+        var _event = new CustomEvent(ImageGrid.enums.IMAGE_PARSED, { 'detail': this._private._grid });
+        this._private._dispatcher.dispatchEvent(_event)
+    }
+
+    var _onImageSet = function(){
+        this._private._image = new Image();
+        var _this = this
+        this._private._image.onload = function() {
+            _this._private._context.drawImage(_this._private._image, 0, 0);
+            _this._private._ImageData = ImageGrid.getImageData(_this._private._context,0,0, _this._private._width, _this._private._height);
+            _makeImageGrid.call(_this);
+        };
+        this._private._image.src = this._private._imageSrc;
+    }
+
+    ///////////
+
     _scope.prototype = {
             getWidth:function(){
-                return _private._width
+                return this._private._width
             },
             setWidth:function(value){
-                _private._width = value;
+                this._private._width = value;
             },
             getHeight:function(){
-                return _private._height;
+                return this._private._height;
             },
             setHeight:function(value){
-                _private._height = value;
+                this._private._height = value;
             },
             getGridSpacing:function(){
-               return  _private._gridSpacing;
+               return  this._private._gridSpacing;
             },
             setGridSpacing:function(value){
-                _private._gridSpacing = value;
-                _private._onGridSpacingChanged.call(this);
+                this._private._gridSpacing = value;
+                _onGridSpacingChanged.call(this);
             },
-            setImage:function(value){
-                _private._image = value
-                _private._onImageSet.call(this);
-            },
+//            setImage:function(value){
+//                this._private._image = value
+//                _onImageSet.call(this);
+//            },
             getTarget:function(){
-              return  _private._opt_target;
+              return  this._private._opt_target;
             },
             setTarget:function(value){
-                _private._opt_target = value;
-                _private._onTargetSet.call(this);
+                this._private._opt_target = value;
+                _onTargetSet.call(this);
             },
             getImage:function(){
-               return  _private._imageSrc;
+               return  this._private._imageSrc;
             },
             setImage:function(value){
-                _private._imageSrc = value;
-                _private._onImageSet();
+                this._private._imageSrc = value;
+                _onImageSet.call(this);
                 this.renderToStage();
             },
+            getDispatcher:function(){
+               return  this._private._dispatcher
+            },
+
             renderToStage:function(){
-                _private._onTargetSet.call(this);
+                _onTargetSet.call(this);
+            },
+            listen:function(evt, callback){
+                this.getDispatcher().addEventListener(evt, callback)
             }
     }
 
@@ -192,6 +206,10 @@ ImageGrid.drawCircle = function(ctx,x ,y,r, colour, border ){
     ctx.setLineWidth(this.guideThickness)
     !border ? ctx.fill() : ctx.stroke();
     ctx.closePath();
+}
+
+ImageGrid.enums = {
+    IMAGE_PARSED:"imageParsed"
 }
 
 
